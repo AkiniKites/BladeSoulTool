8cb506312e8f8e927c1b97dee27c72b575bcfe80
AkiniKites <akinikites@gmail.com>
2021-07-08 19:41:11 -0400

    Add export tool

---
+++ VS_GUI/ExportTool/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExportTool
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Invalid number of arguments");
+                return 1;
+            }
+
+            int pid = int.Parse(args[0]);
+            int timeout = int.Parse(args[1]);
+
+            int w = int.Parse(args[2]);
+            int l = int.Parse(args[3]);
+
+            string output = args[4];
+
+            var hWnd = WaitForWindow(pid, timeout);
+            ResizeWindow(hWnd, w, l);
+
+            Thread.Sleep(250); //wait for repaint
+            
+            var img = GetScreenshot(hWnd);
+            img.Save(output, ImageFormat.Png);
+
+            CloseWindow(hWnd);
+
+            return 0;
+        }
+
+        static IntPtr WaitForWindow(int pid, int timeout)
+        {
+            Process p = null;
+            if (!WaitFor(timeout, () =>
+            {
+                var children = GetAllChildProcesses(pid);
+                if (!children.Any())
+                    return false;
+
+                p = Process.GetProcessById(children[0]);
+                return true;
+            }))
+            {
+                throw new InvalidOperationException("Unable to find child process with pid: " + pid);
+            }
+
+            IntPtr hWnd = IntPtr.Zero;
+            if (!WaitFor(timeout, () =>
+            {
+                if (p.MainWindowHandle == IntPtr.Zero)
+                    return false;
+                hWnd = p.MainWindowHandle;
+                return true;
+            }))
+            {
+                throw new InvalidOperationException("Unable to find window for process with pid: " + pid);
+            }
+
+            return hWnd;
+        }
+
+        static int[] GetAllChildProcesses(int pid)
+        {
+            var searcher = new ManagementObjectSearcher(
+                $"select ProcessId from Win32_Process where ParentProcessId={pid}");
+            var results = searcher.Get();
+
+            return results.Cast<ManagementBaseObject>().Select(x => (int)(uint)x["ProcessId"]).ToArray();
+        }
+
+        static bool WaitFor(int timeout, Func<bool> action)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedMilliseconds < timeout)
+            {
+                if (action())
+                    return true;
+                Thread.Sleep(100);
+            }
+
+            return false;
+        }
+
+        static void ResizeWindow(IntPtr hWnd, int width, int height)
+        {
+            if (!Win32.GetWindowRect(hWnd, out var rect))
+                throw new InvalidOperationException("Unable to find window area for hWnd: " + hWnd);
+            Win32.MoveWindow(hWnd, rect.X, rect.Y, width, height, false);
+        }
+
+        static Bitmap GetScreenshot(IntPtr hWnd)
+        {
+            if (!Win32.GetClientRect(hWnd, out var rect))
+                throw new InvalidOperationException("Unable to find window area for hWnd: " + hWnd);
+
+            var img = new Bitmap(rect.Width, rect.Height);
+            using (Graphics g = Graphics.FromImage(img))
+            {
+                var hDC = g.GetHdc();
+
+                try
+                {
+                    Win32.PrintWindow(hWnd, hDC, Win32.PW_CLIENTONLY);
+                }
+                finally
+                {
+                    g.ReleaseHdc(hDC);
+                }
+            }
+
+            return img;
+        }
+
+        static void CloseWindow(IntPtr hWnd)
+        {
+            Win32.SendMessage(hWnd, Win32.WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+        }
+    }
+}
---
+++ VS_GUI/ExportTool/Win32.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportTool
+{
+    public class Win32
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hwnd, IntPtr hdcBlt, uint nFlags);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr DeleteDC(IntPtr hDc);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+            
+            public int X
+            {
+                get => Left;
+                set => Left = value;
+            }
+            public int Y
+            {
+                get => Top;
+                set => Top = value;
+            }
+            public int Width
+            {
+                get => Right - Left;
+                set => Right = Left + value;
+            }
+            public int Height
+            {
+                get => Bottom - Top;
+                set => Top = Bottom + value;
+            }
+        }
+
+        public const uint PW_CLIENTONLY = 0x1;
+
+        public const UInt32 WM_CLOSE = 0x0010;
+    }
+}
